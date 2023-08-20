4dc86cb0cf58601365b52a77022fe17bff66bd81
Tushar123 <jain0735@gmail.com>
2023-08-21 01:16:34 +0530

    fix: user schema

--- controllers/addproduct.js
+++ controllers/addproduct.js
@@ -2,8 +2,8 @@ const Product = require("../models/productSchema")
 
 async function savedata(req, res) {
     try {
-        const { brand, name, rating, review, image, price, breadcrumbs,index,user } = req.body;
-        const data = new Product({click:"interested", name: name, brand: brand, rating: rating, review: review, image: image, price: price, breadcrumbs: breadcrumbs,index:index, user:user});
+        const { brand, name, rating, review, image, price, breadcrumbs,index} = req.body;
+        const data = new Product({click:"interested", name: name, brand: brand, rating: rating, review: review, image: image, price: price, breadcrumbs: breadcrumbs,index:index});
         await data.save();
 
         res.status(200).send('koko')
--- models/productSchema.js
+++ models/productSchema.js
@@ -1,10 +1,6 @@
 const mongoose =require("mongoose")
 const {Schema} = mongoose
 const ProductSchema = new Schema({
-    user: {
-        type: Object,
-        required: true
-    },
     click: {
         type: String,
         required: true
