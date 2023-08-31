f22d653fcf10ec9c316b2706198f0126002f9be3
tushar <jain0735@gmail.com>
2023-08-31 19:08:42 +0530

    fix: environment variables

--- controllers/getpurchasedProduct.js
+++ controllers/getpurchasedProduct.js
@@ -3,10 +3,10 @@ const Product = require("../models/productSchema")
 async function getLatestPurchasedProducts(req, res) {
     try {
       const purchasedProducts = await Product.find({ click: "purchased" })
-        .sort({ createdAt: -1 }) // Sorting by createdAt in descending order to get the latest products first
-        .limit(5); // Limiting the result to 5 products
+        .sort({ createdAt: -1 }) 
+        .limit(5); 
         const interestedProducts = await Product.find({ click: "interested" })
-        .sort({ createdAt: -1 }) // Sorting by createdAt in descending order to get the latest products first
+        .sort({ createdAt: -1 }) 
         .limit(5);
 
       res.json({'interested': interestedProducts, 'purchased': purchasedProducts});
